8157bb4ac01959d7b24d4c0de2e9e5d6f5409cd2
Jim Kyle <jim@jwkyle.com>
2023-08-11 11:41:05 -0500

    add Batterstatcast model

---
+++ app/models/batterstatcast.rb
@@ -0,0 +1,2 @@
+class Batterstatcast < ApplicationRecord
+end
---
+++ db/migrate/20230811164002_create_batterstatcasts.rb
@@ -0,0 +1,21 @@
+class CreateBatterstatcasts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :batterstatcasts do |t|
+      t.string :last_name
+      t.string :first_name
+      t.integer :player_id
+      t.integer :year
+      t.decimal :xba
+      t.decimal :xslg
+      t.decimal :woba
+      t.decimal :xwoba
+      t.decimal :xobp
+      t.decimal :xiso
+      t.decimal :exit_velocit_avg
+      t.decimal :launch_angle_avg
+      t.decimal :barrel_batted_rate
+
+      t.timestamps
+    end
+  end
+end
